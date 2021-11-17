5f0926a76fec59e903111754232ea7c9b879e2bf
Harsh Kumar <hadron43@yahoo.com>
2021-11-17 13:00:27 +0530

    initialize server

---
+++ flask-server/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask.globals import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def emoji():
+    print(request.args)
+    return {
+        "emoji": ["😂", "😒", "😐"],
+        "confidence": [1, 0.9, 0.02]
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
