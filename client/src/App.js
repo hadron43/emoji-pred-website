b4166022cc4a1e1617dfa3df37c425ca79d8939d
Harsh Kumar <hadron43@yahoo.com>
2021-11-18 00:19:25 +0530

    client: navbar intialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
+import React, {useState} from 'react';
 import './App.css';
 import Particles from 'react-tsparticles';
 import {} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Navigation from './components/Navigation';
+
 const config = require('./particles-config.json');
 
 function App() {
+  const [animation, setAnimation] = useState(false);
+
   return (
-    <div className="App vh-100 bg-black" >
-      <Particles
-        params={config} />
-    </div>
+    <>
+      <div className='particles-background bg-blue-dark' >
+        <Particles className={`${animation ? 'visible' : 'invisible'}`}
+          params={config} />
+      </div>
+      <div className="container py-2 text-light min-vh-100 bg-white bg-opacity-10">
+        <Navigation animation={animation} setAnimation={setAnimation} />
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import {Navbar, NavLink, NavbarBrand, NavbarText, NavbarToggler, Collapse, Button,
+Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+
+function Navigation({animation, setAnimation}) {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const radios = [
+        { name: 'Off', value: '1' },
+        { name: 'On', value: '2' },
+    ];
+
+    return(
+        <div>
+            <Navbar
+                color="light"
+                expand="md"
+                full
+                light
+                className={`rounded px-2 ${collapsed ? 'rounded-pill' : 'rounded-3'}`}
+            >
+                <NavbarBrand href="/">
+                EmojiPred
+                </NavbarBrand>
+                <NavbarToggler onClick={() => setCollapsed(!collapsed)} />
+                <Collapse isOpen={!collapsed} navbar>
+                <Nav
+                    className="me-auto"
+                    navbar
+                >
+                    <NavItem>
+                    <NavLink href="/components/">
+                        Components
+                    </NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink href="">
+                        GitHub
+                    </NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown
+                    inNavbar
+                    nav
+                    >
+                    <DropdownToggle
+                        caret
+                        nav
+                    >
+                        Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem>
+                        Option 1
+                        </DropdownItem>
+                        <DropdownItem>
+                        Option 2
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                        Reset
+                        </DropdownItem>
+                    </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+                <NavbarText>
+                    <Button className="rounded-pill"
+                        outline={!animation}
+                        onClick={() => setAnimation(!animation)}
+                        color={`${!animation ? 'danger' : 'success'}`}>
+                        <i class={`bi bi-camera-video${animation ? '' : '-off'}`}></i>
+                    </Button>
+                </NavbarText>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navigation;
